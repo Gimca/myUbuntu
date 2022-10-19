7d455056e5e8320c4847e7f7f5e010ada09074c5
George Gkimtsas <gimtsas@hotmail.com>
2022-10-19 16:43:50 +0300

    v.0.1.2

--- drivers.sh
+++ drivers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Cannon
-gunzip ~/Downloads/myUbuntu/cnijfilter-mx470series-4.10-1-deb.tar.gz
-sudo bash ~/Downloads/myUbuntu/cnijfilter-mx470series-4.10-1-deb/install.sh
+cd ~/Downloads/myUbuntu/cnijfilter-mx470series-4.10-1-deb
+sudo make install
+sudo bash install.sh
 # Brother
-gunzip ~/Downloads/myUbuntu/linux-brprinter-installer-2.2.3-1.gz
 sudo bash ~/Downloads/myUbuntu/linux-brprinter-installer-2.2.3-1
