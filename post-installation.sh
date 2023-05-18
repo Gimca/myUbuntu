e9e58ab770be378d5cba3b30226e0d7fd9bffe41
George Gkimtsas <gimtsas@hotmail.com>
2023-05-18 13:53:41 +0300

    v0.3.1 - minor adjustments

--- post-installation.sh
+++ post-installation.sh
@@ -1,6 +1,6 @@
 # VSCODE
 sudo apt update && sudo apt upgrade -y
-sudo snap install --classic code -y # or code-insiders
+sudo snap install --classic code # or code-insiders
 # GIT
 sudo apt install git -y
 git config --global user.email "gimtsas@hotmail.com"
@@ -19,13 +19,13 @@ sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common wget -y
 sudo wget -O- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-edge.gpg
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main' | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
-sudo apt update
+sudo apt update -y
 sudo apt install microsoft-edge-stable -y
 # COMMS
 wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
 sudo apt install ./viber.deb -y
 sudo snap install --classic skype
-sudo snap install teams
+sudo snap install teams-for-linux
 sudo snap install discord
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
@@ -39,7 +39,7 @@ sudo apt install insync -y
 # Python
 sudo apt install python3-pip python3-venv -y
 # Postgres
-sudo apt install postgresql postgresql-contrib -y
+sudo apt install postgresql postgresql-contrib postgis -y
 # Anydesk
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
 echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
@@ -50,19 +50,19 @@ sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
 sudo apt install wireguard -y
 # Install apps
 sudo apt install gnome-tweaks gnome-shell-extensions -y
-sudo apt install virtualbox -y
 sudo apt install qgis librecad -y
-sudo snap install dbeaver-ce pencil-snap-demo onlyoffice-desktopeditors obs-studio vlc pdfarranger clamav -y
+sudo apt install virtualbox clamtk pdfarranger xsane imagemagick -y
+sudo snap install dbeaver-ce pencil-snap-demo onlyoffice-desktopeditors obs-studio vlc 
 # windows burner
-sudo add-apt-repository ppa:tomtomtom/woeusb -y
-sudo apt update -y; sudo apt upgrade -y;
-sudo apt install woeusb woeusb-frontend-wxgtk -y
+#sudo add-apt-repository ppa:tomtomtom/woeusb -y
+#sudo apt update -y; sudo apt upgrade -y;
+#sudo apt install woeusb woeusb-frontend-wxgtk -y
 # Languages
 # sudo apt-get install language-pack-el language-pack-gnome-el language-pack-el-base language-pack-gnome-el-base
 # Remove apps
 sudo apt remove --purge libreoffice* -y
 sudo apt purge gnome-sudoku gnome-mines gnome-mahjongg aisleriot remmina evince ubuntu-report popularity-contest apport whoopsie rhythmbox -y
-sudo apt remove simple scan -y
+sudo apt purge simple-scan -y
 # Final update
 sudo apt update -y
 sudo apt upgrade -y
